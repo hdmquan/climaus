6308d25db57d984096635b82e6965dd4ff38ee1b
Alan Huynh <hdmquan@outlook.com>
2025-05-14 10:23:20 +1000

    feat(tailwind): Set up and tested

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    compatibilityDate: "2024-11-01",
-    devtools: { enabled: true }
+  compatibilityDate: '2025-05-14',
+  css: ['@/assets/css/tailwind.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
 })
